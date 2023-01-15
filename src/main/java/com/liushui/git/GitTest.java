37482d04eb116472211f9d43339eea8e1507f3dd
liushui <adcaoe@126.com>
2023-01-15 19:59:05 +0800

    hot-fix commit1

--- src/main/java/com/liushui/git/GitTest.java
+++ src/main/java/com/liushui/git/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("lll");
         System.out.println("lll");
+        System.out.println("hhh");
+        System.out.println("ggh");
     }
 }
